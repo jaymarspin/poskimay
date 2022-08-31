82d0b8f962e099aaff4fb571fba3487acb809712
jaymar daligdig <jaydaligdig123@gmail.com>
2022-08-31 10:04:03 +0800

    jgcg

---
+++ offline-version/electron/capacitor.config.ts
@@ -0,0 +1,36 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jeep.app.ionic.angular',
+  appName: 'angular-sqlite-app-starter',
+  webDir: 'www',
+  bundledWebRuntime: false,
+//  hideLogs: true,
+  plugins: {
+    CapacitorSQLite: {
+      iosDatabaseLocation: 'Library/CapacitorDatabase',
+      iosIsEncryption: true,
+      iosKeychainPrefix: 'angular-sqlite-app-starter',
+      iosBiometric: {
+        biometricAuth: false,
+        biometricTitle : "Biometric login for capacitor sqlite"
+      },
+      androidIsEncryption: true,
+      androidBiometric: {
+        biometricAuth : true,
+        biometricTitle : "Biometric login for capacitor sqlite",
+        biometricSubTitle : "Log in using your biometric"
+      },
+      electronWindowsLocation: "C:\\ProgramData\\CapacitorDatabases",
+      electronMacLocation: "/Volumes/Development_Lacie/Development/CapacitorDatabases",
+      electronLinuxLocation: "Databases"
+    }
+  }
+};
+
+export default config;
+/*
+
+
+
+*/
